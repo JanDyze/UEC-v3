c23632d8977cb1fbf820aa6d1a5133389d7b5b5e
Your Name <you@example.com>
2025-06-01 18:15:37 +0800

    update be

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const attendanceRoutes = require('./routes/attendance');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' })); // Vite default port
+app.use(express.static(path.join(__dirname, '../build'))); 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});// Vite default port
 app.use(express.json());
 app.use(helmet());
 app.use(
